c8341a122d7514a5c9290ecf83d8930cb1a0544a
hhb <2859168358@qq.com>
2024-06-05 11:25:14 +0800

    fix：修改了归还书籍的功能

--- app/Business/AdminBusiness.php
+++ app/Business/AdminBusiness.php
@@ -21,10 +21,10 @@ class AdminBusiness
         LibraryUser::query()->where('name_number', $name_number)->firstOrFail();
         $libraryBook = LibraryBook::query()->where('book_number', $book_number)->firstOrFail();
         if ($libraryBook->book_num <= 0) {
-            LibraryBook::query()->where('book_number', $book_number)->update(['book_status'=>'0']);
+            LibraryBook::query()->where('book_number', $book_number)->update(['book_status' => '0']);
             return false;
         } else {
-            LibraryBook::query()->where('book_number', $book_number)->update(['book_num'=>$libraryBook->book_num -1]);
+            LibraryBook::query()->where('book_number', $book_number)->update(['book_num' => $libraryBook->book_num - 1]);
             BorrowBook::query()->create([
                 'name_number' => $name_number,
                 'book_number' => $book_number,
@@ -42,17 +42,15 @@ class AdminBusiness
      * @param string $book_number
      * @return bool
      */
-    public function return(string $name_number, string $book_number):bool
+    public function return(string $name_number, string $book_number): bool
     {
-        $borrow_book=BorrowBook::query()->where('name_number',$name_number)->where('book_number',$book_number)->firstOrFail();
-        if (Carbon::now()->between($borrow_book->borrow_date,$borrow_book->due_date)){
-            $borrow_book->update(['return_date'=>Carbon::now()]);
-            $book=LibraryBook::query()->where('book_number',$book_number)->firstOrFail();
-            $book->update(['book_number'=>$book->book_number+1]);
-            return true;
-        }
+        $borrow_book = BorrowBook::query()->where('name_number', $name_number)->where('book_number', $book_number)->firstOrFail();
+        if ($borrow_book->return_status == 0)
+          {
+              return $this->returnBook($borrow_book->date,$borrow_book->due_date,$borrow_book,$book_number);
+          }
         else{
-            return false;
+            return $this->returnBook($borrow_book->due_date,$borrow_book->return_date,$borrow_book,$book_number);
         }
     }
 
@@ -61,8 +59,9 @@ class AdminBusiness
      * @param string $searchTerm
      * @return array|string[]
      */
-    public function search(string $searchTerm):array
+    public function search(Request $request): array
     {
+        $searchTerm=$request->input('query');
         // 如果没有搜索关键词，返回空结果
         if (empty($searchTerm)) {
             return
@@ -85,7 +84,7 @@ class AdminBusiness
      * @param Request $request
      * @return string[]
      */
-    public function add(Request $request):array
+    public function add(Request $request): array
     {
         LibraryBook::query()->create($request->all());
         return [
@@ -98,18 +97,47 @@ class AdminBusiness
      * @param Request $request
      * @return string[]
      */
-    public function update(Request $request):array
+    public function update(Request $request): array
     {
-        LibraryBook::query()->where('book_number',$request->book_number)->update($request->all());
+        LibraryBook::query()->where('book_number', $request->book_number)->update($request->all());
         return [
             'mes' => '修改成功',
         ];
     }
-    public function delete(Request $request):array
+
+    /**
+     * 删除书籍
+     * @param Request $request
+     * @return string[]
+     */
+    public function delete(Request $request): array
     {
-        LibraryBook::query()->where('book_number',$request->book_number)->delete();
+        LibraryBook::query()->where('book_number', $request->book_number)->delete();
         return [
             'mes' => '删除成功',
         ];
+    }
+
+    /**
+     * 归还书籍
+     * @param int $date1
+     * @param int $date2
+     * @param $borrow_book
+     * @param $book_number
+     * @return bool
+     */
+    public function returnBook(int $date1, int $date2, $borrow_book, $book_number): bool
+    {
+        if (Carbon::now()->between($date1, $date2)) {
+            $borrow_book->update(['return_date' => Carbon::now()]);
+            $book = LibraryBook::query()->where('book_number', $book_number)->firstOrFail();
+            $book->update(['book_number' => $book->book_number + 1]);
+            $borrow_book->delete();
+            return true;
+        }
+        else{
+            $borrow_book->update(['return_date'=>Carbon::now()->addDays(14)->toDateString(),'return_status'=>rand(1,9)]);
+            return false;
+        }
     }
 }
